8661819e4cf4e1950c147151f4d488873f028228
Sherzod1610 <sherzodrasuljonov086@gmail.com>
2023-05-22 04:23:39 +0500

    finished

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -4,7 +4,13 @@ import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
-        Comparator<String> comparator = (o1, o2) -> Integer.compare(o2.length(), o1.length());
+        Comparator<String> comparator = (o1, o2) -> {
+            if (o1.length() < o2.length())
+                return 1;
+            if (o1.length() == o2.length())
+                return o2.compareTo(o1);
+            return -1;
+        };
         List<String> list = new ArrayList<>();
         Set<String> set = projects.keySet();
         for (String s : set) {
